61c978f6faac944b6ae6c737482c56b24c54ce02
younessz <younesszarhloul@gmail.com>
2018-11-20 23:32:18 +0100

    Update function count_change (add twitter query code)

--- touristR/R/plot_count_change.R
+++ touristR/R/plot_count_change.R
@@ -1,66 +1,85 @@
 
 
 
+
+
 plot_count_change <- function(keyword) {
 
 
 
-  # load required libraries/throw an error message if not possible
-  # convert arguments to vector
-  packages <- c("tidyverse", "twitteR","scales")
-  #package "latex2exp" allows to add Latex math symbols to the plot
-
-  # start loop to determine if each package is installed
-  for (package in packages) {
-    # if package is installed locally, load
-    if (package %in% rownames(installed.packages()))
-    {
-      do.call('library', list(package))
-
-      # if package is not installed locally, throw error message
-    } else {
-      stop(
-        cat("Please make sure that all the following packages are installed: ",
-            packages, sep = "\n"))
-    }
-
-  }
 
 
 
-
-
-library(twitteR)
   consumer_key <- 'ugjqg8RGNvuTAL1fEiNtw'
   consumer_secret <- '4WjuEbP6QLUN2DwDzyTwmdMES6fgnOsS65fWxpT8I'
-  access_token <- '76887198-JA3xCVO1vvQMqMDiIobWKKGQxYKSB0CV2lI2PZ7GL'
+  access_token <-
+    '76887198-JA3xCVO1vvQMqMDiIobWKKGQxYKSB0CV2lI2PZ7GL'
   access_secret <- 'IvzlVOC8KkIaMR5s5K4u2IXbxKQv7EcUSvy2bnaru8gKz'
-  options(httr_oauth_cache=TRUE)
+  options(httr_oauth_cache = TRUE)
 
   setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+  #
+  #
+  library(twitteR)
 
 
-  hashtag <- paste( "#",keyword , sep="")
 
 
- searchTwitter(
 
-   hashtag, n=10,lang = "en",resultType="mixed"),
+  input <- paste("#", keyword,sep="")
+
+  geocode_id <- keyword
+
+library(tidyverse)
+  library(ggmap)
+
+  df <- geocode(location =
+
+                  geocode_id ,
+
+                source = "dsk",messaging=FALSE) %>% as.tibble() %>% rename("long" = lon) %>% mutate(city =
+                                                                                      geocode_id) %>% select(city, long, lat)
 
 
-  since= lubridate::ymd(Sys.Date()),
- #
-     until= (lubridate::ymd(Sys.Date()) -30)
+
+
+  #run query for all data per  month going from past 6 month until today
+
+
+  searchTwitter(
+    input,
+    n = 10,
+    lang = "en",
+    resultType = "mixed",
+    since = as.character ( (lubridate::ymd(Sys.Date() )  - 30) )   ,
+    until = as.character ( lubridate::ymd(Sys.Date()  )       ),
+
+    geocode = paste(df$lat, df$long, "40mi", sep=",")
+
+
+
+
+
+
+
+  )
 
 
 
 
 
 }
+
 #needed to specify the geocode to avoid getting irrelevant content
 #then input this into the twitter search page
 
 
 
-plot_count_change(london)
+plot_count_change(keyword = "lausanne")
+
+
+
+
+
+
 
