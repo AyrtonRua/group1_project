1b0d937bc196193941c045b2096a6e03e02c8dbb
younessz <younesszarhloul@gmail.com>
2018-11-30 18:53:08 +0100

    update Shiny ui.R (improve layout)

--- touristR/inst/shiny_touristR/ui.R
+++ touristR/inst/shiny_touristR/ui.R
@@ -1,74 +1,44 @@
+ui <- fixedPage(
+
+
+  fixedRow(titlePanel("Welcome to touristR!")),
+
+  fixedRow(title="touristR Shiny Output",
+           column(8,
+                  leaflet::leafletOutput("mymap",height = 700)
+
+           ),
 
 
 
-#' @import shiny
-#' @import shinydashboard
-#' @import leaflet
+           column(4,
 
 
-header <- dashboardHeader(title="Welcome to touristR dear traveller!",titleWidth=350)
+                  shiny::helpText( "Create an interactive tourist friendly city hot-spot guide using Twitter data.")  ,
+
+                  shiny::textInput(inputId= "choosecity", label="Choose a city",placeholder="London maybe?!"),
 
 
 
-sidebar <- dashboardSidebar(
 
 
-  helpText("Create an interactive tourist friendly city hot-spot guide."),
+                  # Input: Selector for choosing dataset ----
+                  selectInput(inputId = "place",
+                              label = "Choose a place:",
+                              choices = c("Monument", "Museum", "Nightlife","Parcs"))
 
-  submitButton(text = "Let's go!" ),
-  textInput(inputId= "choosecity", label="Choose a city",placeholder="London maybe?!"),
 
-  # checkboxes
-  checkboxInput(inputId= "Restaurant", label = "Restaurant", value = FALSE),
 
-  checkboxInput(inputId= "Hotel", label = "Hotel", value = FALSE),
+           )
 
-  checkboxInput(inputId= "Nightlife", label = "Nightlife", value = FALSE),
 
-  checkboxInput(inputId= "Parcs", label = "Parcs and attractions", value = FALSE),
 
-  checkboxInput(inputId= "Surprise", label = "Surprise me!", value = FALSE)
+
+
+
+
+
 
 
   )
-
-
-
-
-
-body <- dashboardBody(
-
-
-  leafletOutput("mymap",height = 700)
-
-
 )
-
-
-
-
-
-
-ui <-  shinyUI(
-
-
-  dashboardPage(header, sidebar, body,skin="red")
-
-
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
