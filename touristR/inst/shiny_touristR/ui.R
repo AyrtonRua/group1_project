35a407499873742ac9f1e6ebe10bca5cfe5979a3
younessz <younesszarhloul@gmail.com>
2018-12-05 12:06:09 +0100

    Add about section in ui.R

--- touristR/inst/shiny_touristR/ui.R
+++ touristR/inst/shiny_touristR/ui.R
@@ -32,32 +32,48 @@ ui <- shinyUI(navbarPage( title= "Welcome to touristR!", id="nav",
 
 
 
-####################
-                    tabPanel("About",
-                             h4("User Manual: ", a("Click Here", href=
-                                                     "http://nbviewer.ipython.org/github/funjo/NYPD_accidents_shiny/blob/master/User%20Manual.pdf")),
+################################################################################
+                    tabPanel(title="About",
                              br(),
-                             h4("Data Source"),
-                             p("Source: ",a("NYPD Motor Vehicle Collisions | NYC Open Data.",href=
-                                              "https://data.cityofnewyork.us/Public-Safety/NYPD-Motor-Vehicle-Collisions/h9gi-nx95")),
-                             p("Description: ","Data Details of Motor Vehicle Collisions in
-                              New York City provided by the Police Department (NYPD)."),
-                             p("Usage: ","Original dataset was downloaded on 07/07/2015,
-                              containing 618,358 accident records from 07/01/2012 to 07/05/2015.
-                              Because of the loading speed concern, this app uses only 10,000 random records
-                              from the original dataset."),
+                             h4("Description:"),
+                             p("Tourist friendly map based on live-fetched data from Twitter."),
                              br(),
-                             h4("Author Information"),
-                             p("Fangzhou Cheng"),
-                             p("Email: fc982@nyu.edu"),
-                             p("Website:", a("http://www.fangzhoucheng.com",href="http://www.fangzhoucheng.com")),
-                             p("Github:", a("http://www.github.com/funjo",href="http://www.github.com/funjo")),
-                             p("LinkedIn:", a("http://www.linkedin.com/in/fangzhoucheng",href="http://www.linkedin.com/in/fangzhoucheng")),
+                             h4("Data Source:"),
+                             p(a("Twitter API",href=
+                                              "https://twitter.com/")),
+
+                             br(),
+                             h4("Authors Information:"),
+                             p("Ayrton Rua: ayrton.gomesmartinsrua@unil.ch"),
+
+                             p("Maurizio Griffo: maurizio.griffo@unil.ch"),
+
+
+                             p("Ali Karray: mohamedali.karray@unil.ch"),
+
+
+                             p("Mohit Mehrotra: mohit.mehrotra@unil.ch"),
+
+
+                             p("Youness Zarhloul: youness.zarhloul@unil.ch"),
+                             br(),
+
+                             h4("Links:"),
+
+
+                             p("Github:", a("https://github.com/AyrtonRua/group1_project")),
+
+                              p("Course website:", a("https://ptds2018.netlify.com")),
+
                              br(),
                              br(),
-                             p("Fangzhou Cheng - Copyright @ 2015, All Rights Reserved")
+                             p("HEC Lausanne, 2018"),
+                             p("Programming tools in data science"),
+                             p("Licence: GNU Licence")
+
+
                     )
-####################
+####################################################################################################
 
 
 
